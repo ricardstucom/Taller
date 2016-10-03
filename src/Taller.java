28d727562db27e5c74c220140a4f1e3a22d3a0e8
46989075y <ricardhuelamo@gmail.com>
2016-10-03 21:06:39 +0200

    me falta obtener clientes

--- src/Taller.java
+++ src/Taller.java
@@ -4,15 +4,17 @@ import java.util.Set;
 import java.util.TreeMap;
 
 public class Taller {
-    private Map<Persona, Coche> reparaciones =
-            new TreeMap<>(Comparator.comparing(Persona::getNumSeguridadSocial));
+    private Map<Persona, Coche> reparaciones =  new TreeMap<>(Comparator.comparing(Persona::getNumss));
 
-    public Coche registrarReparacion(Persona persona, Coche coche){
+    public void registrarReparacion(Persona persona, Coche coche){
+        reparaciones.put(persona,coche);
     }
 
     public Coche obtenerCoche(Persona persona){
+        return reparaciones.get(persona);
     }
 
     public Set<Persona> obtenerClientes(){
+
     }
 }
